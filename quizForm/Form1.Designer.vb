ede24ad52c952dbb223895f7283cf736ab82d9c7
GaMeOv3r <GaMeOv3r@DESKTOP-LTVNT53>
2023-10-28 02:14:38 -0700

    Add project files.

---
+++ quizForm/Form1.Designer.vb
@@ -0,0 +1,103 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnSubmit = New Button()
+        Label1 = New Label()
+        lblQuestion = New Label()
+        lblResult = New Label()
+        txtAnswer = New TextBox()
+        SuspendLayout()
+        ' 
+        ' btnSubmit
+        ' 
+        btnSubmit.BackColor = Color.IndianRed
+        btnSubmit.FlatStyle = FlatStyle.Flat
+        btnSubmit.Location = New Point(241, 284)
+        btnSubmit.Name = "btnSubmit"
+        btnSubmit.Size = New Size(190, 63)
+        btnSubmit.TabIndex = 0
+        btnSubmit.Text = "Submit"
+        btnSubmit.UseVisualStyleBackColor = False
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Times New Roman", 15.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Label1.Location = New Point(305, 22)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(114, 24)
+        Label1.TabIndex = 1
+        Label1.Text = "Online Quiz"
+        ' 
+        ' lblQuestion
+        ' 
+        lblQuestion.AutoSize = True
+        lblQuestion.BackColor = Color.LightGreen
+        lblQuestion.Font = New Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        lblQuestion.Location = New Point(241, 83)
+        lblQuestion.Name = "lblQuestion"
+        lblQuestion.Size = New Size(144, 21)
+        lblQuestion.TabIndex = 2
+        lblQuestion.Text = "Questions go here"
+        ' 
+        ' lblResult
+        ' 
+        lblResult.AutoSize = True
+        lblResult.BackColor = Color.LightGreen
+        lblResult.Location = New Point(241, 123)
+        lblResult.Name = "lblResult"
+        lblResult.Size = New Size(0, 15)
+        lblResult.TabIndex = 3
+        ' 
+        ' txtAnswer
+        ' 
+        txtAnswer.Location = New Point(241, 198)
+        txtAnswer.Multiline = True
+        txtAnswer.Name = "txtAnswer"
+        txtAnswer.Size = New Size(103, 35)
+        txtAnswer.TabIndex = 4
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.LightGreen
+        ClientSize = New Size(800, 450)
+        Controls.Add(txtAnswer)
+        Controls.Add(lblResult)
+        Controls.Add(lblQuestion)
+        Controls.Add(Label1)
+        Controls.Add(btnSubmit)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblQuestion As Label
+    Friend WithEvents lblResult As Label
+    Friend WithEvents txtAnswer As TextBox
+End Class
---
+++ quizForm/Form1.vb
@@ -0,0 +1,53 @@
+﻿Public Class Form1
+
+    ' Array to store quiz questions and answers
+    Dim questions() As String = {"What is the capital of France?",
+        "Which planet is known as the Red Planet?",
+        "How many continents are there?"}
+    Dim answers() As String = {"PARIS", "MARS", "7"}
+
+    ' Initialize the userResponses array to match the number of questions
+    Dim userResponses(questions.Length - 1) As String
+
+    ' Index to keep track of the current question being displayed
+    Dim currentQuestionIndex As Integer = 0
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lblQuestion.Text = questions(currentQuestionIndex)
+
+    End Sub
+
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        Dim userAnswer As String = txtAnswer.Text
+        userAnswer = userAnswer.ToUpper
+
+        userResponses(currentQuestionIndex) = userAnswer
+
+        If userAnswer = answers(currentQuestionIndex) Then
+            lblResult.Text = "Correct!"
+        Else
+            lblResult.Text = "Incorrect. The correct answer is: " & answers(currentQuestionIndex)
+        End If
+
+        currentQuestionIndex += 1
+
+        If currentQuestionIndex < questions.Length Then
+            lblQuestion.Text = questions(currentQuestionIndex)
+            txtAnswer.Clear()
+        Else
+            ' Quiz completed. Calculate the score and show a message box.
+            Dim correctCount As Integer = 0
+            For i = 0 To questions.Length - 1
+                If userResponses(i) = answers(i) Then
+                    correctCount += 1
+                End If
+
+
+
+            Next
+            Dim resultMessage As String = "Quiz completed. You got " & correctCount & " out of " & questions.Length & " questions correct."
+            MessageBox.Show(resultMessage, "Quiz Completed")
+            Me.Close()
+        End If
+    End Sub
+End Class
